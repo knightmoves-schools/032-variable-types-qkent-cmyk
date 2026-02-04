01b4c6af4e119936b993faf9161977a10f1d810c
Kent1an <qkent@anamosa.k12.ia.us>
2026-02-04 16:24:04 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+// index.js
+const animalName = "Cat";
+const height = 25;
+const isIndoorPet = true;
 
+// Display only the animalName in the element with id="result"
+document.getElementById("result").innerHTML = animalName;
